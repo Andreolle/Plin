b5e31ce5e4eba394adb17b286277afeadc2d4987
Andreolle Dantas <andreolle18@gmail.com>
2019-03-22 23:36:46 -0300

    removing unsed constructor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react'
 import Menu from '../components/Menu'
 
 class Home extends Component {
-	constructor(props) {
-    super(props)
-	}
-
 	render() {
 		return (
 			<div className="home">
