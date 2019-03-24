b75031da146cc0f3d36340f206c5b5fed874f193
Andreolle Dantas <andreolle18@gmail.com>
2019-03-24 17:54:43 -0300

    fix menu test

--- src/test/Menu.spec.js
+++ src/test/Menu.spec.js
@@ -5,18 +5,14 @@ import Menu from '../components/Menu'
 
 configure({adapter: new Adapter()})
 describe('Menu component', () => {
-	it('Menu should has class "open" when passed prop isOpen equals true.', () => {
-		const menu = shallow(<Menu isOpen={true} />)
-		expect(menu.find('.menu').hasClass('menu--open')).toEqual(true)
-	})
-
-	it('Menu should initiate close by default.', () => {
+	it('Menu should initiate closed.', () => {
 		const menu = shallow(<Menu />)
 		expect(menu.find('.menu').hasClass('menu--open')).toEqual(false)
 	})
 
-	it('Menu should has no class "open" when passed prop isOpen equals false.', () => {
-		const menu = shallow(<Menu isOpen={false} />)
-		expect(menu.find('.menu').hasClass('menu--open')).toEqual(false)
+	it('When Menu state isOpen is true, should have "menu--open" class.', () => {
+		const menu = shallow(<Menu />)
+		menu.setState({ isOpen: true });
+		expect(menu.find('.menu').hasClass('menu--open')).toEqual(true)
 	})
 })
